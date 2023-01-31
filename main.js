2b74918553fd5e9253da5593dfb397786ab642c5
lineiika <avramenko.liana@gmail.com>
2023-01-31 22:10:05 +0200

    add 31/10

---
+++ main.js
@@ -0,0 +1,14 @@
+
+const array = [1, 2, 3, 4, 5, 6, 7];
+console.log (array);
+
+
+   function removeElement(array, index){
+   array.splice(4, 1);
+   }
+  
+
+
+removeElement(array);
+
+console.log(array);
